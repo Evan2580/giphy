a3bd29356a4dad9d6df81b8bf1b2735d2627b2b4
unknown <evan2580@gmail.com>
2019-03-29 22:21:55 -0400

    final working version

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,6 +10,7 @@ $.ajax({
 }).then(function(response) {
   console.log(response);
   var len = response.data.length;
+
       for(var i = 0; i < len; i++) {
       //GET rate
       var pictureRated = response.data[i].rating;
@@ -18,15 +19,23 @@ $.ajax({
       var imgURL = response.data[i].images.fixed_height.url;
 
       //create div for rate
-      var ratedDiv = $("<div id='rated'>");
+      var ratedDiv = $("<div>");
+      ratedDiv.attr("class", "rated animated fadeInLeft");
       //insert rate information into the div
       ratedDiv.html("Rated: " + pictureRated);
 
       // Creating an element to hold the image
       var image = $("<img>").attr("src", imgURL);
+      image.attr("data-state", "animate");
+      image.attr("data-still", response.data[i].images.fixed_height_still.url);
+      image.attr("data-animate", response.data[i].images.fixed_height.url);
+      image.attr("class", "img-responsive animated fadeInRight gif");
+
 
       //append
-      $("#add-giphys").append(ratedDiv).append(image);
+      var gifContainer = $("<div class = 'gifContainer'>");
+        gifContainer.append(ratedDiv).append(image);
+      $("#add-giphys").append(gifContainer);
 
     }
 });
@@ -42,7 +51,7 @@ function buttons() {
     btn.addClass("giphy-btn");
 
     btn.attr("data-name", searchTerm[i]);
-
+    // btn.attr("class","zoomIn")
     btn.html(searchTerm[i]);
 
     $("#buttons").append(btn);
@@ -62,4 +71,18 @@ $("#add-gifs").on("click", function(event) {
 
   buttons();
 });
+
 $("#buttons").on("click", ".giphy-btn", giphyButtons);
+
+$(document).on("click", '.gif', function(){
+  var state = $(this).attr("data-state");
+  // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+  // Then, set the image's data-state to animate
+  // Else set src to the data-still value
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+}});
